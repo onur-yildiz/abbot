32b68e5a9c635d0c69a53c50735239b862f10272
onur-yildiz <onur.yildizcp13@hotmail.com>
2021-04-09 18:41:14 +0300

    create constants file for generic messages

--- constants/messages.ts
+++ constants/messages.ts
@@ -1,27 +1,29 @@
 export const JOIN_CHANNEL_STOP =
-  'You have to be in the same voice channel to stop the music.'
+  "You have to be in the same voice channel to stop the music.";
 export const JOIN_CHANNEL_SKIP =
-  'You have to be in the same voice channel to skip the music.'
+  "You have to be in the same voice channel to skip the music.";
 export const JOIN_CHANNEL_CLEAR =
-  'You have to be in the same voice channel to clear the queue.'
+  "You have to be in the same voice channel to clear the queue.";
 export const JOIN_CHANNEL_GENERIC =
-  'You have to be in the same voice channel to do that'
-export const JOIN_CHANNEL_PLAY =
-  'You need to be in a voice channel to play music!'
+  "You have to be in the same voice channel to do that";
+export const JOIN_CHANNEL_PLAY = "You need to be in a voice channel first.";
 
-export const QUEUE_EMPTY_SKIP = 'There is more items in the queue.'
-export const QUEUE_EMPTY_CLEAR = 'The queue is already empty.'
-export const QUEUE_CLEARED = 'The queue is cleared.'
-export const QUEUE_EMPTY = 'The queue is empty.'
+export const QUEUE_EMPTY_SKIP = "There is no more items in the queue.";
+export const QUEUE_EMPTY_CLEAR = "The queue is already empty.";
+export const QUEUE_CLEARED = "The queue is cleared.";
+export const QUEUE_EMPTY = "The queue is empty.";
 export const QUEUE_CHANNEL_CLEAR =
-  'You have to be in the same voice channel to clear the queue.'
+  "You have to be in the same voice channel to clear the queue.";
 
 export const PERMISSIONS_PLAY =
-  'I need the permissions to join and speak in the voice channel'
+  "I need the permissions to join and speak in the voice channel";
 
-export const TEST_COMMAND_NOT_VALID = 'TEST: command not valid.'
+export const TEST_COMMAND_NOT_VALID = "TEST: command not valid.";
+export const TEST_EXECUTION_ERROR =
+  "TEST: there was an error trying to execute that command!";
 
-export const BOT_NOT_IN_CHANNEL = 'I am not active in a channel.'
+export const BOT_NOT_IN_CHANNEL = "I am not active in a channel.";
+export const BOT_NOT_IN_SAME_CHANNEL = "I am in another voice channel";
 
 // const QUEUE_ADDED_MSG = (title: string) => `${title} has been added to the queue!`
 // const PLAYING_MSG = (title: string) => `Playing: **${title}**`
