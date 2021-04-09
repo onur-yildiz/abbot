bbfc2121d52495c534288209cda36fd443538424
onur-yildiz <onur.yildizcp13@hotmail.com>
2021-04-09 18:40:20 +0300

    rename extension file, create new extensions

---
+++ extensions/string.ts
@@ -0,0 +1,47 @@
+interface String {
+  toBold(): string;
+  toInlineCodeBg(): string;
+  toCodeBg(): string;
+  toCodeBgCs(): string;
+  toSpoiler(): string;
+}
+
+Object.defineProperty(String.prototype, "toBold", {
+  value: function toBold() {
+    return `\*\*${this}\*\*`;
+  },
+  writable: true,
+  configurable: true,
+});
+
+Object.defineProperty(String.prototype, "toInlineCodeBg", {
+  value: function toInlineCodeBg() {
+    return `\`${this}\``;
+  },
+  writable: true,
+  configurable: true,
+});
+
+Object.defineProperty(String.prototype, "toCodeBg", {
+  value: function toCodeBg() {
+    return `\`\`\`${this}\`\`\``;
+  },
+  writable: true,
+  configurable: true,
+});
+
+Object.defineProperty(String.prototype, "toCodeBgCs", {
+  value: function toCodeBgCs() {
+    return `\`\`\`cs\n${this}\n\`\`\``;
+  },
+  writable: true,
+  configurable: true,
+});
+
+Object.defineProperty(String.prototype, "toSpoiler", {
+  value: function toSpoiler() {
+    return `\|\|${this}\|\|`;
+  },
+  writable: true,
+  configurable: true,
+});
