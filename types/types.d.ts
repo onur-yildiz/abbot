768710f619c9ae9b11a4ed30ef31c73c3fb667ec
onur-yildiz <onur.yildizcp13@hotmail.com>
2021-04-09 18:22:10 +0300

    create seperate type file

---
+++ types/types.d.ts
@@ -0,0 +1,33 @@
+declare module "discord.js" {
+  type QueueContract = {
+    textChannel: TextChannel | DMChannel | NewsChannel;
+    voiceChannel: VoiceChannel;
+    connection: VoiceConnection;
+    songs: Array<Song>;
+    volume: number;
+    playing: boolean;
+    enableGreeting: boolean;
+  };
+
+  type Command = {
+    name: string;
+    description: string;
+    guildOnly: boolean;
+    aliases: Array<string>;
+    usage: string;
+    permissions?: PermissionResolvable;
+    cooldown?: number;
+    args?: boolean;
+    execute: Function;
+  };
+
+  type Cooldowns = Collection<string, Collection<string, number>>;
+}
+
+declare type Song = {
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+  desc: string;
+  author: string;
+};
