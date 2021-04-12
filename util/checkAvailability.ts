523741c51285f879db4741cdddb0752d260758e2
onur-yildiz <onur.yildizcp13@hotmail.com>
2021-04-12 19:06:27 +0300

    refactor under same if

--- util/checkAvailability.ts
+++ util/checkAvailability.ts
@@ -7,9 +7,11 @@ import {
 export const checkAvailability = (message: Message): string | null => {
   if (message.guild.voice == null || message.guild.voice.channel == null)
     return BOT_NOT_IN_CHANNEL;
-  if (message.member.voice == null || message.member.voice.channel == null)
-    return JOIN_CHANNEL_GENERIC;
-  if (message.member.voice.channel.id != message.guild.voice.channel.id)
+  if (
+    message.member.voice == null ||
+    message.member.voice.channel == null ||
+    message.member.voice.channel.id != message.guild.voice.channel.id
+  )
     return JOIN_CHANNEL_GENERIC;
   return null;
 };
--- util/deleteQueue.ts
+++
@@ -1,6 +0,0 @@
-import { Guild } from "discord.js";
-import { guildContracts } from "../app";
-
-export const deleteQueue = (guild: Guild): boolean => {
-  return guildContracts.delete(guild.id);
-};
