06ee3bad3a386ae602e3d6058e8960663104c4f5
onur-yildiz <onur.yildizcp13@hotmail.com>
2021-04-12 19:17:03 +0300

    add a new horn implementation which supports valid audio file urls or preinstalled mp3 files

--- commands/sound/horn.ts
+++ commands/sound/horn.ts
@@ -1,46 +1,59 @@
-import { Command, Message } from "discord.js";
+import fs from "fs";
+import { Command, GuildData, Message } from "discord.js";
 import { TEST_EXECUTION_ERROR } from "../../constants/messages";
 import { checkUserInAChannel } from "../../util/checkUserInAChannel";
 import { connect } from "../../util/connect";
-import { getCommandContent } from "../../util/getCommandContent";
-import { getOrInitQueue } from "../../util/getOrInitQueue";
+import { getAndUpdateGuildData } from "../../util/getAndUpdateGuildData";
+import { urlReachable } from "../../util/urlReachable";
 
 export = <Command>{
   name: "horn",
   aliases: ["h", "say"],
   description: "Play a sound. Ex. [~horn ww].",
-  usage: "[audioName]",
+  usage: "[audio name]",
   guildOnly: true,
-  async execute(message: Message) {
+  args: Args.required,
+  async execute(message: Message, args: string[]) {
     const error = checkUserInAChannel(message);
     if (error) return message.channel.send(error.toBold());
 
-    const commandContent = getCommandContent(message.content);
-    const queueContract = getOrInitQueue(
+    const commandContent = args[1];
+    const guildData = getAndUpdateGuildData(
       message.guild,
       message.channel,
       message.member.voice.channel
     );
 
-    if (queueContract.playing)
+    if (guildData.queueActive)
       return message.channel.send(
         `I am already playing music.\nYou have to clear song queue to play audio.`.toBold()
       );
 
-    // TODOimplement your own.
-    //! excluded in public repo.
-    //const audioPath = pickAudioPath()
-    const audioPath = "https://www.youtube.com/watch?v=PRc2vx4xTVM";
-
     try {
-      await connect(queueContract);
-      const dispatcher = queueContract.connection
+      let audioPath = "";
+      const audios = fs.readdirSync("./assets/audio");
+      if (audios.includes(`${commandContent}.mp3`))
+        audioPath = `./assets/audio/${commandContent}.mp3`;
+      else if (guildData.audioAliases.has(commandContent)) {
+        audioPath = guildData.audioAliases.get(commandContent);
+        if (!(await urlReachable(audioPath))) return;
+      } else {
+        return message.reply(
+          "No audio named " +
+            `${commandContent}`.toInlineCodeBg() +
+            " was found."
+        );
+      }
+
+      console.log(audioPath);
+      await connect(guildData);
+      const dispatcher = guildData.connection
         .play(audioPath)
         .on("finish", () => {
           responseMessage.delete();
         })
         .on("error", (error) => console.error(error));
-      dispatcher.setVolumeLogarithmic(queueContract.volume);
+      dispatcher.setVolumeLogarithmic(guildData.volume);
       const responseMessage = await message.channel.send(
         `Playing **${commandContent}**`
       );
