bda3e32af9f96b683eee3881884133eab87d61bd
onur-yildiz <onur.yildizcp13@hotmail.com>
2021-04-12 19:11:24 +0300

    rename leave to disconnect, and minor refactoring.

---
+++ commands/music/disconnect.ts
@@ -0,0 +1,29 @@
+import { Command, Message } from "discord.js";
+import { checkAvailability } from "../../util/checkAvailability";
+import { deleteGuild } from "../../util/deleteGuild";
+import { getAndUpdateGuildData } from "../../util/getAndUpdateGuildData";
+
+export = <Command>{
+  name: "disconnect",
+  aliases: ["l", "quit", "dc", "leave"],
+  description: "Disconnects the bot from the voice channel.",
+  usage: "",
+  args: Args.none,
+  guildOnly: true,
+  execute(message: Message) {
+    const error = checkAvailability(message);
+    if (error) return message.channel.send(error.toBold());
+
+    const guildData = getAndUpdateGuildData(
+      message.guild,
+      message.channel,
+      message.member.voice.channel
+    );
+
+    if (guildData.connection.dispatcher != null)
+      guildData.connection.dispatcher.destroy();
+    guildData.connection.disconnect();
+    deleteGuild(message.guild);
+    return message.channel.send("Disconnected.".toBold());
+  },
+};
