a25770e0f2b0850c236ba5d67652585ea47b58db
onur-yildiz <onur.yildizcp13@hotmail.com>
2021-04-12 18:45:42 +0300

    removed getOrInit function and made two seperate functions for 'get and update' and 'init'

---
+++ util/initGuildData.ts
@@ -0,0 +1,40 @@
+import { Guild, GuildData, VoiceChannel } from "discord.js";
+import { guilds } from "../app";
+import { getGuildSettings } from "../db/dbHelper";
+
+require("dotenv").config();
+const defaultPrefix = process.env.PREFIX;
+
+export const initGuildData = async (
+  guild: Guild,
+  voiceChannel?: VoiceChannel
+): Promise<GuildData> => {
+  if (guilds.has(guild.id)) return guilds.get(guild.id);
+  let guildData = <GuildData>{
+    textChannel: null,
+    voiceChannel: voiceChannel ? voiceChannel : null,
+    connection: null,
+    songs: [],
+    volume: 1,
+    queueActive: false,
+    greetingEnabled: true,
+    audioAliases: new Map(),
+    prefix: defaultPrefix,
+  };
+
+  try {
+    const guildSettings = await getGuildSettings(guild);
+    if (guildSettings) {
+      if (guildSettings.greetingEnabled)
+        guildData.greetingEnabled = guildSettings.greetingEnabled;
+      if (guildSettings.audioAliases)
+        guildData.audioAliases = guildSettings.audioAliases;
+      if (guildSettings.prefix) guildData.prefix = guildSettings.prefix;
+    }
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+  guilds.set(guild.id, guildData);
+  return guildData;
+};
