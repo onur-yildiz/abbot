9f452bd786c9d3a10990c44f5220e41c22f2a3e3
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2021-04-16 20:41:56 +0300

    unit testing on util funcs

---
+++ __tests__/util/checker.test.ts
@@ -0,0 +1,248 @@
+import { Message, Permissions } from "discord.js";
+import {
+  BOT_NOT_IN_CHANNEL,
+  BOT_NOT_IN_SAME_CHANNEL,
+  JOIN_CHANNEL_GENERIC,
+  PERMISSIONS_PLAY,
+} from "../../constants/messages";
+import {
+  checkVoiceChannelAvailability,
+  checkUserInAChannel,
+} from "../../util/checker";
+
+describe("checker", () => {
+  let message: Message;
+  const mockPermissionsFor = jest.fn((): Readonly<Permissions> => null);
+
+  beforeEach(() => {
+    message = null;
+  });
+
+  describe("#checkVoiceChannelAvailability", () => {
+    it("should return null if both in same channel and bot has perms", () => {
+      message = ({
+        member: {
+          voice: {
+            channel: {
+              id: "same-id",
+            },
+          },
+        },
+        guild: {
+          voice: {
+            channel: {
+              id: "same-id",
+            },
+          },
+        },
+      } as unknown) as Message;
+      const perms = new Permissions().add("CONNECT").add("SPEAK");
+      message.member.voice.channel.permissionsFor = mockPermissionsFor.mockReturnValue(
+        perms
+      );
+      expect(checkVoiceChannelAvailability(message)).toBeNull();
+    });
+
+    it("should return BOT_NOT_IN_SAME_CHANNEL if in different channels", () => {
+      message = ({
+        member: {
+          voice: {
+            channel: {
+              id: "same-id",
+            },
+          },
+        },
+        guild: {
+          voice: {
+            channel: {
+              id: "diff-id",
+            },
+          },
+        },
+      } as unknown) as Message;
+      const perms = new Permissions().add("CONNECT").add("SPEAK");
+      message.member.voice.channel.permissionsFor = mockPermissionsFor.mockReturnValue(
+        perms
+      );
+      expect(checkVoiceChannelAvailability(message)).toBe(
+        BOT_NOT_IN_SAME_CHANNEL
+      );
+    });
+
+    describe("user not in a channel", () => {
+      test("if user voiceState is falsy", () => {
+        message = ({
+          member: {},
+          guild: {
+            voice: {
+              channel: {
+                id: "same-id",
+              },
+            },
+          },
+        } as unknown) as Message;
+        expect(message.member.voice).toBeFalsy();
+        expect(checkVoiceChannelAvailability(message)).toBe(
+          JOIN_CHANNEL_GENERIC
+        );
+      });
+
+      test("if user voice channel is falsy", () => {
+        message = ({
+          member: {
+            voice: {},
+          },
+          guild: {
+            voice: {
+              channel: {
+                id: "same-id",
+              },
+            },
+          },
+        } as unknown) as Message;
+        expect(checkVoiceChannelAvailability(message)).toBe(
+          JOIN_CHANNEL_GENERIC
+        );
+      });
+    });
+
+    describe("false permissions", () => {
+      test("if no `CONNECT` perm", () => {
+        message = ({
+          member: {
+            voice: {
+              channel: {
+                id: "same-id",
+              },
+            },
+          },
+          guild: {
+            voice: {
+              channel: {
+                id: "same-id",
+              },
+            },
+          },
+        } as unknown) as Message;
+        const perms = new Permissions().add("VIEW_GUILD_INSIGHTS").add("SPEAK");
+        message.member.voice.channel.permissionsFor = mockPermissionsFor.mockReturnValue(
+          perms
+        );
+        expect(checkVoiceChannelAvailability(message)).toBe(PERMISSIONS_PLAY);
+      });
+
+      test("if no `SPEAK` perm", () => {
+        message = ({
+          member: {
+            voice: {
+              channel: {
+                id: "same-id",
+              },
+            },
+          },
+          guild: {
+            voice: {
+              channel: {
+                id: "same-id",
+              },
+            },
+          },
+        } as unknown) as Message;
+        const perms = new Permissions().add("CONNECT").add("VIEW_AUDIT_LOG");
+        message.member.voice.channel.permissionsFor = mockPermissionsFor.mockReturnValue(
+          perms
+        );
+        expect(checkVoiceChannelAvailability(message)).toBe(PERMISSIONS_PLAY);
+      });
+
+      test("if no `SPEAK` and `CONNECT` perm", () => {
+        message = ({
+          member: {
+            voice: {
+              channel: {
+                id: "same-id",
+              },
+            },
+          },
+          guild: {
+            voice: {
+              channel: {
+                id: "same-id",
+              },
+            },
+          },
+        } as unknown) as Message;
+        const perms = new Permissions().add("USE_VAD").add("VIEW_AUDIT_LOG");
+        message.member.voice.channel.permissionsFor = mockPermissionsFor.mockReturnValue(
+          perms
+        );
+        expect(checkVoiceChannelAvailability(message)).toBe(PERMISSIONS_PLAY);
+      });
+    });
+
+    describe("bot not in a channel", () => {
+      test("if bot voice state is falsy", () => {
+        message = ({
+          member: {
+            voice: {
+              channel: {
+                id: "same-id",
+              },
+            },
+          },
+          guild: {},
+        } as unknown) as Message;
+        const perms = new Permissions().add("CONNECT").add("SPEAK");
+        message.member.voice.channel.permissionsFor = mockPermissionsFor.mockReturnValue(
+          perms
+        );
+        expect(checkVoiceChannelAvailability(message)).toBe(BOT_NOT_IN_CHANNEL);
+      });
+
+      test("if bot voice channel is falsy", () => {
+        message = ({
+          member: {
+            voice: {
+              channel: {
+                id: "same-id",
+              },
+            },
+          },
+          guild: { voice: {} },
+        } as unknown) as Message;
+        const perms = new Permissions().add("CONNECT").add("SPEAK");
+        message.member.voice.channel.permissionsFor = mockPermissionsFor.mockReturnValue(
+          perms
+        );
+        expect(checkVoiceChannelAvailability(message)).toBe(BOT_NOT_IN_CHANNEL);
+      });
+    });
+  });
+
+  describe("#checkUserInAChannel", () => {
+    it("should return null if user in a channel", () => {
+      message = ({
+        member: {
+          voice: {
+            channel: {},
+          },
+        },
+      } as unknown) as Message;
+      expect(checkUserInAChannel(message)).toBeNull();
+    });
+    describe("user not in a channel", () => {
+      test("if user voice state is falsy", () => {
+        message = ({
+          member: {},
+        } as unknown) as Message;
+        expect(checkUserInAChannel(message)).toBe(JOIN_CHANNEL_GENERIC);
+      });
+      test("if user voice channel is falsy", () => {
+        message = ({
+          member: {},
+        } as unknown) as Message;
+        expect(checkUserInAChannel(message)).toBe(JOIN_CHANNEL_GENERIC);
+      });
+    });
+  });
+});
---
+++ __tests__/util/commandParser.test.ts
@@ -0,0 +1,90 @@
+import { defaultPrefix } from "../../global/globals";
+import { getCommandContent, getCommandName } from "../../util/commandParser";
+
+describe("commandParser", () => {
+  describe("getCommandContent", () => {
+    describe("with default prefix", () => {
+      it("should return expectedly", () => {
+        const commandMessage = ".play    Frank's choice    ";
+        const expectedValue = "Frank's choice";
+
+        expect(getCommandContent(commandMessage, defaultPrefix)).toBe(
+          expectedValue
+        );
+      });
+    });
+
+    describe("with custom prefix", () => {
+      const customPrefix = "tst ";
+      it("should return expectedly", () => {
+        const commandMessage = "tst play    Frank's choice    ";
+        const expectedValue = "Frank's choice";
+
+        expect(getCommandContent(commandMessage, customPrefix)).toBe(
+          expectedValue
+        );
+      });
+    });
+  });
+
+  describe("getCommandName", () => {
+    describe("with default prefix", () => {
+      it("should return expectedly", () => {
+        const commandMessage = ".play    Frank's choice    ";
+        const expectedValue = "play";
+
+        expect(getCommandName(commandMessage, defaultPrefix)).toBe(
+          expectedValue
+        );
+      });
+
+      it("should return as lowercase", () => {
+        const commandMessage = ".hOrN    Frank's choice    ";
+        const expectedValue = "horn";
+
+        expect(getCommandName(commandMessage, defaultPrefix)).toBe(
+          expectedValue
+        );
+      });
+
+      it("should return '' if wrong prefix", () => {
+        const commandMessage = "pls hOrN    Frank's choice    ";
+        const expectedValue = "";
+
+        expect(getCommandName(commandMessage, defaultPrefix)).toBe(
+          expectedValue
+        );
+      });
+    });
+
+    describe("with custom prefix", () => {
+      const customPrefix = "tst ";
+      it("should return expectedly", () => {
+        const commandMessage = "tst pLaY    Frank's choice    ";
+        const expectedValue = "play";
+
+        expect(getCommandName(commandMessage, customPrefix)).toBe(
+          expectedValue
+        );
+      });
+
+      it("should return as lowercase", () => {
+        const commandMessage = "tst hOrN    Frank's choice    ";
+        const expectedValue = "horn";
+
+        expect(getCommandName(commandMessage, customPrefix)).toBe(
+          expectedValue
+        );
+      });
+
+      it("should return '' if wrong prefix", () => {
+        const commandMessage = "!!hOrN    Frank's choice    ";
+        const expectedValue = "";
+
+        expect(getCommandName(commandMessage, customPrefix)).toBe(
+          expectedValue
+        );
+      });
+    });
+  });
+});
