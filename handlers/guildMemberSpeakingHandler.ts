6b6cb49a7f34a5b81878fd550b91953b1f82c6a9
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2021-05-07 16:43:25 +0300

    only speak when user starts speaking

--- handlers/guildMemberSpeakingHandler.ts
+++ handlers/guildMemberSpeakingHandler.ts
@@ -1,12 +1,15 @@
-import { GuildMember } from "discord.js";
+import { GuildMember, Speaking } from "discord.js";
 import { logger } from "../global/globals";
 import { fetchGuildData } from "../util/guildActions";
 
-export const guildMemberSpeakingHandler = async (guildMember: GuildMember) => {
+export const guildMemberSpeakingHandler = async (
+  guildMember: GuildMember,
+  speaking: Speaking
+) => {
   try {
     const guildData = await fetchGuildData(guildMember.guild);
 
-    if (guildData.annoyanceList.has(guildMember.id)) {
+    if (guildData.annoyanceList.has(guildMember.id) && speaking.bitfield) {
       guildData.connection?.play(guildData.annoyanceList.get(guildMember.id));
     }
   } catch (error) {
