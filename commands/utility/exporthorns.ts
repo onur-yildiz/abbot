cc347b1d58f4c3cd25fb1895b779543cfec23516
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2021-05-08 17:20:03 +0300

    implement exporthorns command

---
+++ commands/utility/exporthorns.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import { stringify } from "yaml";
+import { Command, Message } from "discord.js";
+import { ERROR_EXECUTION_ERROR } from "../../constants/messages";
+import DBHelper from "../../db/DBHelper";
+import { logger } from "../../global/globals";
+
+export = <Command>{
+  name: "exporthorn",
+  aliases: ["exph"],
+  description: "Export saved horns.",
+  usage: "",
+  isGuildOnly: true,
+  args: Args.none,
+  async execute(message: Message) {
+    try {
+      const guildSettings = await DBHelper.getGuildSettings(message.guild, {
+        audioAliases: 1,
+      });
+
+      const filePath = `horns_${message.guild.id}.yaml`;
+      fs.writeFileSync(filePath, stringify(guildSettings.audioAliases));
+      await message.channel.send({
+        files: [filePath],
+      });
+      fs.rmSync(filePath);
+    } catch (error) {
+      logger.error(error);
+      message.reply(ERROR_EXECUTION_ERROR);
+    }
+  },
+};
