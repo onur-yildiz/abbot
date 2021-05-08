09d13d31823fe2ee9743efb6a7be3241b5b437c5
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2021-05-08 19:30:17 +0300

    workaround for max 2000 char message limit

--- commands/utility/args.ts
+++ commands/utility/args.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { Command, Message } from "discord.js";
 import { ERROR_EXECUTION_ERROR, NO_SAVED_ARGS } from "../../constants/messages";
 import DBHelper from "../../db/DBHelper";
@@ -30,7 +31,29 @@ export = <Command>{
         return message.channel.send(NO_SAVED_ARGS.toBold());
 
       data.sort();
-      return message.channel.send(data.join(", ").toInlineCodeBg());
+      let args = data.join(", ");
+
+      // !!
+      // Max allowed length is 2000 for a message.
+      // When sending at the end, toInlineCodeBg() func adds "`" to beginning and end of the string.
+      // Because of that we are checking for max length of 1998.
+      if (args.length >= 1998) {
+        let index = 0;
+        let messages = new Array<string>();
+        while (true) {
+          const newLinePos = args.indexOf(", ", index + 1971); // max 25 chars + ', ';
+          if (newLinePos === -1) {
+            messages.push(args.slice(index + 2)); // ', ' 2 chars
+            break;
+          }
+          messages.push(args.slice(index, newLinePos) + "\n"); // ', ' 2 chars
+          index = newLinePos;
+        }
+
+        for (const msg of messages) {
+          await message.channel.send(msg.toInlineCodeBg());
+        }
+      } else message.channel.send(args.toInlineCodeBg());
     } catch (error) {
       message.reply(ERROR_EXECUTION_ERROR.toBold());
       logger.error(error);
