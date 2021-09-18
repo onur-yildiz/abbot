858d8fd9774ded21281832a2b0b06587e7ce87c9
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2021-09-18 16:55:28 +0300

    implemented pagination for queue

--- commands/queue/queue.ts
+++ commands/queue/queue.ts
@@ -1,10 +1,16 @@
-import Discord, { Command, Message } from "discord.js";
+import Discord, {
+  CollectorFilter,
+  Command,
+  Message,
+  MessageReaction,
+  ReactionEmoji,
+  User,
+} from "discord.js";
 
 import { ERROR_EXECUTION_ERROR, QUEUE_EMPTY } from "../../constants/messages";
 import { logger } from "../../global/globals";
 import { checkVoiceChannelAvailability } from "../../util/checker";
 import { fetchGuildData } from "../../util/guildActions";
-import { awaitDone } from "../../util/messageUtil";
 
 export = <Command>{
   name: "queue",
@@ -31,23 +37,59 @@ export = <Command>{
       const currentSong = guildData.songs[0];
       queue.push({
         name: `:musical_note: Now Playing: ${currentSong.title}`,
-        value: `${currentSong.duration} • ${currentSong.channel}`,
+        value: `${currentSong.duration} • ${currentSong.channel} • [🔗](${currentSong.url})`,
       });
       guildData.songs.slice(1).forEach((song, index) =>
         queue.push({
           name: `${index + 1}: ${song.title}`,
-          value: `${song.duration} • ${song.channel}`,
+          value: `${song.duration} • ${song.channel} • [🔗](${song.url})`,
         })
       );
 
-      const embed = new Discord.MessageEmbed()
-        .setColor("#222222")
-        .setTitle("Queue")
-        .setThumbnail(guildData.songs[0].thumbnailUrl)
-        .addFields(...queue)
-        .setTimestamp();
+      const createQueueEmbed = (page: number) => {
+        return new Discord.MessageEmbed()
+          .setColor("#222222")
+          .setTitle("Queue")
+          .setThumbnail(guildData.songs[0].thumbnailUrl)
+          .addFields(queue.slice((page - 1) * 6, page * 6))
+          .setTimestamp();
+      };
+
+      let page: number = 1;
+      let embed = createQueueEmbed(page);
       const responseMessage = await message.channel.send(embed);
-      awaitDone(responseMessage, message);
+      let rprev: MessageReaction = await responseMessage.react("⬅");
+      let rnext: MessageReaction = await responseMessage.react("➡");
+
+      const filter: CollectorFilter = (reaction: MessageReaction, user: User) =>
+        (reaction.emoji.name === "➡" || reaction.emoji.name === "⬅") &&
+        user.id === message.author.id;
+      const options = { time: 60000 };
+      const collector = responseMessage.createReactionCollector(
+        filter,
+        options
+      );
+
+      collector.on("collect", async (reaction: MessageReaction, user: User) => {
+        if (reaction.emoji.name === "➡") {
+          rnext?.users.remove(user.id);
+          if (Math.ceil(queue.length / 6) === page) return;
+          page++;
+          responseMessage.edit(createQueueEmbed(page));
+        } else if (reaction.emoji.name === "⬅") {
+          rprev?.users.remove(user.id);
+          if (page === 1) return;
+          page--;
+          responseMessage.edit(createQueueEmbed(page));
+        }
+      });
+
+      collector.on("end", () => {
+        if (!responseMessage.deleted) {
+          rnext?.remove();
+          rprev?.remove();
+        }
+      });
     } catch (error) {
       logger.error(ERROR_EXECUTION_ERROR);
     }
