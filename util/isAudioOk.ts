7153236b2052fce46a7e05343a12529849f91cda
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2021-10-02 14:26:28 +0300

    removed current length check solution for isAudioOk since it cannot get duration of every audio.

--- util/isAudioOk.ts
+++ util/isAudioOk.ts
@@ -1,35 +1,31 @@
-// import https from "https";
-// import { resolve } from "path";
-import fetch from "node-fetch";
-import createEstimator, { FetchDataReader } from "mp3-duration-estimate";
+import https from "https";
+// import fetch from "node-fetch";
+// import createEstimator, { FetchDataReader } from "mp3-duration-estimate";
 import { logger } from "../global/globals";
 
 export const isAudioOk = async (url: string): Promise<boolean> => {
   try {
-    const estimator = createEstimator(new FetchDataReader(fetch));
-    const duration = await estimator(url);
-    if ((!duration && duration !== 0) || duration > 15) return false;
-    return true;
+    // const estimator = createEstimator(new FetchDataReader(fetch));
+    // const duration = await estimator(url);
+    // if ((!duration && duration !== 0) || duration > 15) return false;
+    // return true;
+    return new Promise((resolve, reject) => {
+      const req = https.get(url);
+
+      req.on("response", async (res) => {
+        req.end();
+        if (res.statusCode !== 200) {
+          resolve(false);
+          return;
+        }
+        resolve(true);
+      });
+
+      req.on("error", (err) => {
+        reject(err);
+      });
+    });
   } catch (error) {
     logger.error(error);
   }
-
-  // return new Promise((resolve, reject) => {
-  //   const req = https.get(url);
-
-  //   req.on("response", async (res) => {
-  //     req.end();
-  //     const estimator = createEstimator(new FetchDataReader(fetch));
-  //     const duration = await estimator(url);
-  //     if (res.statusCode !== 200 || duration > 10) {
-  //       resolve(false);
-  //       return;
-  //     }
-  //     resolve(true);
-  //   });
-
-  //   req.on("error", (err) => {
-  //     reject(err);
-  //   });
-  // });
 };
