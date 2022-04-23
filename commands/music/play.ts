89b0e98dc8c6c9710f178107328a0704170430ce
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-04-23 14:08:21 +0300

    refactor: move duration parsing functions to in a seperate file in utils folder.
    It will also be used in seek command.

--- commands/music/play.ts
+++ commands/music/play.ts
@@ -14,6 +14,7 @@ import {
   checkUserInAChannel,
 } from "../../util/checker";
 import { connectToVoiceChannel, fetchGuildData } from "../../util/guildActions";
+import { hhmmss, hhmmssToSeconds } from "../../util/durationParser";
 
 export = <Command>{
   name: "play",
@@ -245,32 +246,6 @@ const fetchPlayable = async (
   }
   if (songs.length === 0) return null;
   return <Playable>{ songs: songs, playlist: playlist };
-};
-
-// convert seconds to hh:mm:ss
-const hhmmss = (durationInSeconds: number): string => {
-  const hours = Math.trunc(durationInSeconds / 3600);
-  const minutes = Math.trunc((durationInSeconds % 3600) / 60);
-  const seconds = Math.trunc(durationInSeconds % 60);
-
-  const fHours: string = hours > 0 ? hours + ":" : "";
-  const fMinutes: string =
-    (hours > 0 ? `${minutes}`.padStart(2, "0") : minutes) + ":";
-  const fSeconds: string = `${seconds}`.padStart(2, "0");
-
-  return fHours + fMinutes + fSeconds;
-};
-
-// convert hh:mm:ss to seconds
-const hhmmssToSeconds = (durationString: string): number => {
-  const sections: string[] = durationString.split(":").reverse();
-  const parsedSections: number[] = sections.map((section) => parseInt(section));
-
-  let eta = 0;
-  if (parsedSections[0]) eta += parsedSections[0];
-  if (parsedSections[1]) eta += parsedSections[1] * 60;
-  if (parsedSections[2]) eta += parsedSections[2] * 3600;
-  return eta;
 };
 
 const calculateEta = (songs: Song[], lastTrackStart: number): string => {
---
+++ util/durationParser.ts
@@ -0,0 +1,25 @@
+// convert seconds to hh:mm:ss
+export const hhmmss = (durationInSeconds: number): string => {
+  const hours = Math.trunc(durationInSeconds / 3600);
+  const minutes = Math.trunc((durationInSeconds % 3600) / 60);
+  const seconds = Math.trunc(durationInSeconds % 60);
+
+  const fHours: string = hours > 0 ? hours + ":" : "";
+  const fMinutes: string =
+    (hours > 0 ? `${minutes}`.padStart(2, "0") : minutes) + ":";
+  const fSeconds: string = `${seconds}`.padStart(2, "0");
+
+  return fHours + fMinutes + fSeconds;
+};
+
+// convert hh:mm:ss to seconds
+export const hhmmssToSeconds = (durationString: string): number => {
+  const sections: string[] = durationString.split(":").reverse();
+  const parsedSections: number[] = sections.map((section) => parseInt(section));
+
+  let eta = 0;
+  if (parsedSections[0]) eta += parsedSections[0];
+  if (parsedSections[1]) eta += parsedSections[1] * 60;
+  if (parsedSections[2]) eta += parsedSections[2] * 3600;
+  return eta;
+};
