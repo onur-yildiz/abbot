c899954a7bf630fd5954b73cc0da926bac0d9d56
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-04-23 18:10:50 +0300

    refactor: moved play function to seperate file in util folder

--- commands/music/play.ts
+++ commands/music/play.ts
@@ -1,5 +1,4 @@
-import { Command, GuildData, Message, MessageEmbed } from "discord.js";
-import ytdl from "ytdl-core";
+import { Command, Message } from "discord.js";
 import {
   NOTHING_TO_PLAY,
   ERROR_EXECUTION_ERROR,
@@ -12,6 +11,7 @@ import {
 import { connectToVoiceChannel, fetchGuildData } from "../../util/guildActions";
 import { fetchPlayable } from "../../util/fetchPlayable";
 import { setupQueue } from "../../util/setupQueue";
+import { play } from "../../util/play";
 
 export = <Command>{
   name: "play",
@@ -65,87 +65,4 @@ export = <Command>{
       message.reply(ERROR_EXECUTION_ERROR);
     }
   },
-};
-
-const play = async (
-  message: Message,
-  guildData: GuildData,
-  seekedSecond: number = 0,
-  startPaused: boolean = false
-) => {
-  const currentSong = guildData.songs[0];
-  const embed = new MessageEmbed()
-    .setColor("#FFD700")
-    .setAuthor("Now Playing 🎶")
-    .setTitle(currentSong.title)
-    .setThumbnail(currentSong.thumbnailUrl)
-    .setDescription(
-      currentSong.desc.length > 100
-        ? currentSong.desc.slice(0, 100) + "..."
-        : currentSong.desc
-    )
-    .setURL(currentSong.url)
-    .addField("Channel", currentSong.channel, true)
-    .addField("Duration", currentSong.duration, true);
-
-  let responseMessage: Message;
-  try {
-    if (seekedSecond === 0) responseMessage = await message.channel.send(embed);
-    const dispatcher = guildData.connection
-      .play(
-        ytdl(currentSong.url, {
-          filter: "audioonly",
-          highWaterMark: 1 << 25,
-          dlChunkSize: 1 << 25,
-          quality: "highestaudio",
-        }),
-        { seek: seekedSecond }
-      )
-      .on("start", () => {
-        if (seekedSecond === 0) guildData.lastTrackStart = Date.now();
-        else guildData.lastTrackStart = Date.now() - seekedSecond * 1000;
-        logger.info(
-          `Play ::: ${currentSong.url} @${message.guild.name}<${message.guild.id}>`
-        );
-      })
-      .on("skip", () => {
-        dispatcher.emit("finish");
-      })
-      .on("seek", (seconds: number) => {
-        if (dispatcher.paused) play(message, guildData, seconds, true);
-        else play(message, guildData, seconds);
-      })
-      // ! dispatcher.resume() works correctly only on node v14.16.1 for discordjs 12.5
-      .on("resume", () => {
-        if (guildData.songs.length === 0) {
-          guildData.isQueueActive = false;
-          // if currentSong is not skipped, resume.
-        } else if (currentSong === guildData.songs[0]) {
-          dispatcher.resume();
-        } else {
-          responseMessage.delete();
-          play(message, guildData);
-        }
-      })
-      .on("finish", () => {
-        responseMessage?.delete();
-        if (guildData.isLoopActive) guildData.songs.push(guildData.songs[0]);
-        guildData.songs.shift();
-        if (guildData.songs.length === 0) guildData.isQueueActive = false;
-        else play(message, guildData);
-      })
-      .on("error", (error) => {
-        responseMessage?.edit("Couldn't play the song. Skipping...");
-        if (guildData.isLoopActive) guildData.songs.push(guildData.songs[0]);
-        guildData.songs.shift();
-        if (guildData.songs.length === 0) guildData.isQueueActive = false;
-        else play(message, guildData);
-        logger.error(error.message);
-      });
-    dispatcher.setVolumeLogarithmic(0.65);
-    if (startPaused) dispatcher.pause();
-  } catch (error) {
-    responseMessage.delete();
-    logger.error(error.message);
-  }
 };
